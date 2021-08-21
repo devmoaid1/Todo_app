86b03504a5ec7e686d86e960e3e1a9aa55c68448
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-08-22 00:03:21 +0800

    final

--- todolist_app/lib/screens/todo_list.dart
+++ todolist_app/lib/screens/todo_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/data_service.dart';
 import '../models/todo.dart';
-import '../models/data.dart';
 
 class TodoListScreen extends StatefulWidget {
   @override
