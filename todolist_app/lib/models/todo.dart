8c17b702973b87ce750c79d518bed97cb0499607
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-08-21 23:49:32 +0800

    todo model

---
+++ todolist_app/lib/models/todo.dart
@@ -0,0 +1,15 @@
+class Todo {
+  String id;
+  String title;
+  bool completed;
+
+  Todo({this.id, this.title, this.completed = false});
+
+  Todo.copy(Todo from) : this(title: from.title, completed: from.completed);
+
+  Todo.fromJson(Map<String, dynamic> json)
+      : this(
+            id: json['id'], title: json['title'], completed: json['completed']);
+
+  Map<String, dynamic> toJson() => {'title': title, 'completed': completed};
+}
